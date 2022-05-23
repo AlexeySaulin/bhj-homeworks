f13c7ada6adb2f5f8b200c7abc1daea4fe31748d
Alexey Saulin <yabaunti@gmail.com>
2022-05-23 19:52:04 +0300

    corrections

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,8 +2,11 @@ const reveals = document.querySelectorAll('.reveal');
 reveals.forEach(element => {
     document.addEventListener('scroll', ()=>{
         const {top, bottom} = element.getBoundingClientRect();
-        if ( bottom > 0 || top < window.innerHeight ) {
-            element.classList.add('reveal_active')
+        if (top < window.innerHeight) {
+            element.classList.add('reveal_active');
+        }
+        if (bottom < 0 || top > window.innerHeight) {
+            element.classList.remove('reveal_active');
         }
     });
 });
