3007d8f3d40a966dfd34c11db74f657645eb428e
Alexey Saulin <yabaunti@gmail.com>
2022-06-02 19:13:46 +0300

    corrected

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,10 +2,24 @@ const tooltip = document.querySelectorAll('.has-tooltip');
 
 tooltip.forEach(element => element.addEventListener('click', (event) => {
     event.preventDefault();
-    target = event.target;
-    const position = {left, bottom} = target.getBoundingClientRect();
-    target.insertAdjacentHTML("afterEnd", 
-    `<div class="tooltip tooltip_active" style="top:${position.bottom}px; left:${position.left}px">
-        ${target.title}
-    </div>`);
+    
+    const tooltipContainer = document.querySelector('.tooltip');
+
+    function createTitle() {
+        target = event.target;
+        const position = {left, bottom} = target.getBoundingClientRect();
+        target.insertAdjacentHTML("afterEnd", 
+        `<div class="tooltip tooltip_active" style="top:${position.bottom}px; left:${position.left}px">
+            ${target.title}
+        </div>`);
+    }
+
+    if (!tooltipContainer) {
+        createTitle();
+    } else {
+        if (tooltipContainer.innerText != target.title) {
+            createTitle();
+        }
+        tooltipContainer.remove();
+    }  
 }));
