cb483850670efadec686c79ad38c6734543fd32e
Alexey Saulin <yabaunti@gmail.com>
2022-06-02 19:53:44 +0300

    corrected

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,23 +3,20 @@ const taskAddBtn = document.getElementById('tasks__add');
 const taskList = document.getElementById('tasks__list');
 
 taskAddBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+  taskInput.value = taskInput.value.trim();
+  event.preventDefault();
+  if (taskInput.value) {
 
-    if (taskInput.value !== '') {
-        taskList.insertAdjacentHTML('afterEnd', `<div class="task">
-        <div class="task__title">
-          ${taskInput.value}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-      </div>`)
-    }
+    taskList.insertAdjacentHTML('afterEnd', `<div class="task">
+    <div class="task__title"> ${taskInput.value} </div>
+    <a href="#" class="task__remove">&times;</a>
+    </div>`);
     taskInput.value = '';
 
     const taskRemoveBtn = document.querySelector('.task__remove');
-
     taskRemoveBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-        taskRemoveBtn.parentElement.remove();
+    event.preventDefault();
+    taskRemoveBtn.parentElement.remove();
     });
-
+  }
 });
